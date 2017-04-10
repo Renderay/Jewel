beb9c2253ce74d6b5f31eed8da2d8137c60cbbfd
Renderay <polupan.andrew@gmail.com>
2017-04-10 20:40:01 +0300

    Заменил фавикон, поставил алерт на отправку формы, сделал цвет шрифта черный

--- php/mail.php
+++ php/mail.php
@@ -1,11 +1,11 @@
-<meta http-equiv='refresh' content='3; url=http://neurallery.com/'>
+<meta http-equiv='refresh' content='1; url=http://neurallery.com/'>
 <meta charset="UTF-8" />
 <?php
 //Получаем данные из глобальной переменной $_GET, так как мы передаем данные методом GET
 //$name = $_GET['name']; // Вытаскиваем имя в переменную
 $email = $_GET['e-mail']; // Вытаскиваем почту в переменную
 $message = "Сообщение от\r\n$email."; // Формируем сообщение, отправляемое на почту
-$to = 'fisher140@yandex.ru'; // Задаем получателя письма
+$to = 'vyatlev@gmail.com'; // Задаем получателя письма
 
 $from = "neurallery.com"; // От кого пришло письмо
 
@@ -13,10 +13,14 @@ $subject = "Письмо с формы сайта http://neurallery.com/"; // З
 
 $headers = "From: $from\r\nReply-To: $to\r\nContent-type: text/html; charset=utf-8\r\n"; // Формируем заголовок письма (при неправильном формировании может ломаться кодировка и т.д.)
 
+
+
+
+
 IF (mail($to, $subject, $message, $headers)) { // При помощи функции mail, отправляем сообщение, проверяя отправилось оно или нет
-echo "<p>Сообщение успешно отправлено</p>"; // Отправка успешна
+echo "<script>alert(\"Thank you! We'll connect you soon\");</script>"; // Отправка успешна
 }
 ELSE {
-echo "<p>Что-то пошло не так, как планировалось</p>"; // Письмо не отправилось
+echo "<script>alert(\"Something has gone wrong\");</script>"; // Письмо не отправилось
 }
 ?>
